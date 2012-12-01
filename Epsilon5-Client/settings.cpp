cad1da50a92dfa973a0994a2d54314fdabf390a8
Denis <denisavvakumov@gmail.com>
2012-12-01 20:26:07 +0400

    Rewrited CheckIniFile

--- Epsilon5-Client/settings.cpp
+++ Epsilon5-Client/settings.cpp
@@ -8,29 +8,20 @@ TSettings::TSettings(QObject *parent)
     : USettings(parent)
 {
     // TODO: Rewrite this function
-    //CheckIniFile();
+    CheckIniFile(); //If file doesn't exist, create it with default settings
     Load("settings.ini");
 }
 
 void TSettings::CheckIniFile(){
-    // Length: 59 / 0x0000003B (bytes)
-    unsigned char rawData[59] =
-    {
-        0x6E, 0x69, 0x63, 0x6B, 0x6E, 0x61, 0x6D, 0x65, 0x3D, 0x62, 0x61, 0x6B, 0x0D, 0x0A, 0x73, 0x65,
-        0x72, 0x76, 0x65, 0x72, 0x2E, 0x61, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x3D, 0x31, 0x32, 0x37,
-        0x2E, 0x30, 0x2E, 0x30, 0x2E, 0x31, 0x0D, 0x0A, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72, 0x2E, 0x70,
-        0x6F, 0x72, 0x74, 0x3D, 0x31, 0x34, 0x35, 0x36, 0x37, 0x0D, 0x0A,
-    } ;
-
     QFile IniFile("settings.ini");
     if (!IniFile.exists()){
         if (IniFile.open(QIODevice::WriteOnly)) {
-            qint64 bytesWritten = IniFile.write(reinterpret_cast<const char*>(rawData), sizeof(rawData));
-                    if (bytesWritten < sizeof(rawData)) {
-                        //error here
-                    }
-                    IniFile.close();
-                }
+            QTextStream stream(&IniFile);
+            stream << "nickname=bak\r" << endl;
+            stream << "server.address=127.0.0.1\r" << endl;
+            stream << "server.port=14567" << endl;
+            IniFile.close();
+        }
     }
 }
 

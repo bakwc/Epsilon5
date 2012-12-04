fc61ddd13dd7d8e8f1d9545ec7782917cd51166d
Makoto Nokata <nokatamakoto@gmail.com>
2012-12-04 19:33:32 +0600

    Little fix. Removed some magic constants

--- Epsilon5-Server/server.cpp
+++ Epsilon5-Server/server.cpp
@@ -5,6 +5,9 @@
 #include "server.h"
 #include "player.h"
 
+const QHostAddress& DEFAULT_SERVER_ADDRESS = QHostAddress::Any;
+const quint16 DEFAULT_SERVER_PORT = 14567;
+
 TServer::TServer(QObject *parent)
     : QObject(parent)
     , Server(new QUdpSocket(this))
@@ -14,11 +17,11 @@ TServer::TServer(QObject *parent)
 }
 
 void TServer::Start() {
-    if (Server->bind(QHostAddress("0.0.0.0"), 14567))
+    if (Server->bind(DEFAULT_SERVER_ADDRESS, DEFAULT_SERVER_PORT))
     {
         this->startTimer(20); // TODO: Remove MN
     } else {
-        throw UException("Can't listen to port 14567");
+        throw UException(QString("Can't listen to port %1").arg(DEFAULT_SERVER_PORT));
     }
 }
 

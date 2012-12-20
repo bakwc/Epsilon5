beafaef64e73b929d93fd57e18d09bf49820dfc1
Denis <denisavvakumov@gmail.com>
2012-12-20 21:01:39 +0400

    Fix previous  settings changes(mistake)

--- Epsilon5-Client/settings.cpp
+++ Epsilon5-Client/settings.cpp
@@ -4,23 +4,6 @@
 #include <QStringList>
 #include "settings.h"
 
-#ifdef Q_OS_WIN32
-
-#include <windows.h>
-#include <winsock2.h>
-
-#endif
-
-#ifdef Q_OS_LINUX
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-
-#endif
-
-
 const char* SETTINGS_FILENAME = "settings.ini";
 const char* DEFAULT_PLAYER_NAME = "player";
 const char* DEFAULT_SERVER_ADDRESS = "127.0.0.1";
@@ -51,73 +34,7 @@ QString TSettings::GetNickname() {
 }
 
 QString TSettings::GetServerAddr() {
-    QString serverAddr = Settings->GetParameter("server.address");
-
-#ifdef Q_OS_WIN32
-    WSADATA wsaData;
-    int iResult;
-    int i = 0;
-    QString v4addr;
-    struct hostent *remoteHost;
-    struct in_addr addr;
-
-    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (iResult != 0) {
-        qDebug() << "WSA Start up failed";
-    }
-
-    remoteHost = gethostbyname("google.ru");
-
-    if (remoteHost->h_addrtype == AF_INET)
-    {
-        while (remoteHost->h_addr_list[i] != 0)
-        { //we take last ip addr in list
-            addr.s_addr = *(u_long *) remoteHost->h_addr_list[i++];
-            v4addr = inet_ntoa(addr);
-            qDebug() << v4addr;
-        }
-    }
-    return v4addr;
-#endif
-
-#ifdef Q_OS_LINUX
-    struct addrinfo hints, *res, *p;
-    int status;
-    char ipstr[INET6_ADDRSTRLEN];
-
-
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC; // IPv4 OR IPv6
-    hints.ai_socktype = SOCK_STREAM;
-
-    if ((status = getaddrinfo(qPrintable(serverAddr), NULL, &hints, &res)) != 0) {
-        qDebug() << serverAddr;
-        qDebug() << "ERROR: getaddrinfo";
-    }
-
-    for(p = res;p != NULL; p = p->ai_next) {
-        void *addr;
-
-
-        // get pointer ipv4 or ipv6
-        if (p->ai_family == AF_INET) { // IPv4
-            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
-            addr = &(ipv4->sin_addr);
-        } else { // IPv6
-            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
-            addr = &(ipv6->sin6_addr);
-        }
-
-        // convert ip to string
-        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
-        QString serverAddr = ipstr;
-        qDebug() << serverAddr;
-    }
-
-    freeaddrinfo(res);
-
-return serverAddr;
-#endif
+    return Settings->GetParameter("server.address");
 }
 
 quint16 TSettings::GetServerPort() {
